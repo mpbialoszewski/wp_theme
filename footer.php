d2e274bb4e8eb3562f3067d02be2c9a591a783ae
mpbialoszewski <m.p.bialoszewski@outlook.com>
2023-09-16 22:05:37 +0100

    dynamic pages for footer and header content

---
+++ footer.php
@@ -0,0 +1,9 @@
+<footer>
+      <h2>Footer</h2>
+
+<?php
+
+    wp_footer(  );
+?>
+</footer>
+
--- front-page.php
+++ front-page.php
@@ -1,31 +1,18 @@
-<!doctype html>
-<html lang="en">
-  <head>
-    <meta charset="UTF-8" />
-    <link rel="preconnect" href="https://fonts.googleapis.com">
-    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-    <link href="https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;1,100;1,200;1,300;1,400;1,500;1,600;1,700&family=Space+Mono&display=swap" rel="stylesheet">
-    <!---Styles-->
-    <?php
-    wp_head();
-    ?>
-
-
-    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-    <title>Seven Living</title>
-  </head>
+  <?php
+  get_header();
+  ?>
+  <title>Seven Livings</title>
   <body>
-    <div class="header-container">
-        <div class="header-title">
-            <h1>Coming Soon</h1>
-        </div>
-    </div>
-  </body>
-  <footer>
-</footer>
+  <div class="header-container">
+      <div class="header-title">
+          <h1>Coming Soon</h1>
+      </div>
+  </div>
 
-<?php
-  wp_footer();
-?>
-</html>
+  <?php
+    get_footer();
+  ?>
+
+
+
 
