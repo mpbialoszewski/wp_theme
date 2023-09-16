c95f036aaf239559345e6493d5ffae1af0530735
mpbialoszewski <m.p.bialoszewski@outlook.com>
2023-09-16 22:05:53 +0100

    adding theme support for WP

--- functions.php
+++ functions.php
@@ -1,4 +1,11 @@
 <?php
+
+//Adds dynamic tag support
+function theme_support(){
+    add_theme_support( 'title-tag' );
+}
+add_action('after_theme_setup', 'theme_support');
+
 //In case the styles from other plugins will be overriding 
 function sevenliving_register_styles(){
     $version = wp_get_theme()->get( 'Version' );
@@ -9,6 +16,8 @@ add_action('wp_enqueue_scripts','sevenliving_register_styles');
 
 function sevenliving_register_scripts(){
     wp_enqueue_script('sevenliving-bootstrap', "https://cdn.jsdelivr.net/npm/bootstrap@3.3.7/dist/js/bootstrap.min.js", array(), '3.3.7','all',true);
+    wp_enqueue_script('sevenliving-popper',"https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.6/dist/umd/popper.min.js", array(), '2.11.6', "all", true);
+    wp_enqueue_script('sevenliving-jquery', get_template_directory_uri( )."./assets/js/main.js", array(), '1.0',true);
 }
 add_action('wp_enqueue_scripts','sevenliving_register_scripts');
 
