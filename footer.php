802fa55615c8e15652c80e5045da90aef17e3f53
mpbialoszewski <m.p.bialoszewski@outlook.com>
2023-09-16 23:33:42 +0100

    Font awesome icons in footer

--- footer.php
+++ footer.php
@@ -1,6 +1,20 @@
 <footer>
       <h2>Footer</h2>
 
+      <div class='contact-information'>
+            <div class='contact-phone'>
+                <i class="fa fa-phone"></i> 0121 468 3264
+            </div>
+        
+            <div class='contact-email'>
+                <i class="fa fa-envelope"></i> hello@sevenliving.com
+            </div>
+
+            <div class='contact-address'>
+                <i class="fa fa-map-marker"></i> 97 Park Lane, London, W1K 7TG
+            </div>
+        </div>
+
 <?php
 
     wp_footer(  );
