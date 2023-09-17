465bce437d83e6bfd553ff92344d9ec13dbe495f
mpbialoszewski <m.p.bialoszewski@outlook.com>
2023-09-17 18:51:15 +0100

    Return blog posts

--- front-page.php
+++ front-page.php
@@ -1,13 +1,17 @@
-  <?php
+<?php
   get_header();
   ?>
   <title>Seven Livings</title>
-  <body>
+
+  <article class="content px-3 py-5 p-md-5">
+
+  <?php
+    while( have_posts(  )){
+     the_post();
+     get_template_part('template_parts/content', 'article' );
+   }
+   ?>
 
   <?php
     get_footer();
   ?>
-
-
-
-
